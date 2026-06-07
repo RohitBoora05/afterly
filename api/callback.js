e5198eaf5f194900b9d59d49b7307768f2620b32
Rohit Boora <useafterly@gmail.com>
2026-06-07 16:15:55 +0000

    fix: add redirect_uri to OAuth token exchange, explicit Vercel build config

--- api/callback.js
+++ api/callback.js
@@ -12,6 +12,7 @@ export default async function handler(req, res) {
       client_id: process.env.GITHUB_CLIENT_ID,
       client_secret: process.env.GITHUB_CLIENT_SECRET,
       code,
+      redirect_uri: `https://${req.headers['host']}/api/callback`,
     }),
   })
 
